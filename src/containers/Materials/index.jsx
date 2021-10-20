a28894c298acc137d86cfde4001a61aeb9daac9f
Sadia Uzair <sadia.uzair@devsinc.com>
2021-10-20 23:17:21 +0500

    deleted console.log statement

--- src/containers/Materials/index.jsx
+++ src/containers/Materials/index.jsx
@@ -5,7 +5,6 @@ export const Materials = () => {
 
   return (
     <div>
-      {console.log('hey')}
       <h2>MATERIAL</h2>
       {Array.from({ length: clothMaterial.materials.length }, (v, i) => (
         <p> {clothMaterial.materials[i].toUpperCase()}</p>
