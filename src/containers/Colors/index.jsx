41cdaeaf869aceef155dbd168f5dcc298472468b
Sadia Uzair <sadia.uzair@devsinc.com>
2021-10-21 10:06:58 +0500

    updated colors option

--- src/containers/Colors/index.jsx
+++ src/containers/Colors/index.jsx
@@ -1,31 +1,33 @@
 import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 
 import { Button, Div } from './styleComponents'
+
 export const Colors = () => {
-  const selectedColors = useSelector(state => state.colorReducer)
+  const dispatch = useDispatch()
+  const selectedColors = useSelector(state => state.cartReducer)
+  const states = useSelector(state => state.clothReducer)
   let [index, setIndex] = useState(0)
 
   useEffect(() => {
-    for (let i = 0; i < selectedColors.sizes.length; i++) {
-      if (selectedColors.sizeId.type == selectedColors.sizes[i].id) {
+    for (let i = 0; i < states.sizes.length; i++) {
+      if (selectedColors.sizeId == states.sizes[i].id) {
         setIndex((index = i))
       }
     }
   }, [selectedColors.count])
 
-  const handleColor = () => {
-    console.log('test')
+  const handleColor = colorId => {
+    dispatch({ type: states.sizes[index].colors[colorId].name })
   }
-
   return (
     <div>
       <h2>SELECT A COLOR</h2>
       <Div className={'d-inline-flex'}>
-        {Array.from({ length: selectedColors.sizes[index].colors.length }, (v, i) => (
+        {Array.from({ length: states.sizes[index].colors.length }, (v, i) => (
           <div key={i}>
             <Button onClick={() => handleColor(i)} className={'text-white m-2'} key={i}>
-              {selectedColors.sizes[index].colors[i].name}
+              {states.sizes[index].colors[i].name}
             </Button>
           </div>
         ))}
--- src/reducers/colorReducer.jsx
+++
@@ -1,9 +0,0 @@
-import { product } from './initialStates'
-
-export const colorReducer = (state = product, action) => {
-  return {
-    ...state,
-    sizeId: action,
-    count: state.count + 1
-  }
-}
